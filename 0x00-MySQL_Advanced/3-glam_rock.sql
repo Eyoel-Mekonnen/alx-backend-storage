099a2731898cd517094eaac96b3a3afb6a7e8447
root <root@DESKTOP-B19RQ5P>
2024-08-25 22:49:55 +0300

    task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,10 @@
 -- finds difference
-SELECT band_name, formed-split as lifespan
-GROUP BY band_name;
+SELECT band_name,
+	CASE
+		WHEN split IS NOT NULL
+			THEN split - formed
+		ELSE 2022 - formed
+	END AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
