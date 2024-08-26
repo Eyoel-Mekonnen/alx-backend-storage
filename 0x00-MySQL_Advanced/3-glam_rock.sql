b733b8df2f29caa712ff4d6754a5c3d9e2b5fc1a
root <root@DESKTOP-3SEIDIF>
2024-08-26 12:10:25 +0300

    task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,10 @@
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- finds difference
+SELECT band_name,
+	CASE
+		WHEN split >= formed
+			THEN split - formed
+		ELSE 2022 - formed
+	END AS lifespan
+FROM metal_bands
+WHERE LOCATE('Glam rock', style)
+ORDER BY lifespan DESC;
