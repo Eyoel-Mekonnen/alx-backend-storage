1b45d8dd326ee1a8375d996b3c9b249346453dd3
root <root@DESKTOP-3SEIDIF>
2024-08-28 16:00:25 +0300

    task7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,14 +6,18 @@ BEGIN
 DECLARE averagescore FLOAT;
 DECLARE userid INT;
 DECLARE counts INT;
-DECLRE 
 SET userid = user_id;
 SELECT SUM(score), COUNT(user_id) INTO averagescore, counts
 FROM corrections
 WHERE user_id = userid;
-
-UPDATE users
-SET average_score = averagescore / counts
-WHERE id = userid;
+IF counts > 0 THEN
+	UPDATE users
+	SET average_score = averagescore / counts
+	WHERE id = userid;
+ELSE 
+	UPDATE users
+	SET average_score = 0
+	WHERE id = userid;
+END IF;
 END $$
 DELIMITER ;
