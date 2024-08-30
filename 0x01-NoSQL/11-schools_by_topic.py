8138f913029cddcb81c4f382637ab40f98fd90ca
root <root@DESKTOP-3SEIDIF>
2024-08-30 19:00:24 +0300

    finddoc

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Update a document."""
+
+
+def def schools_by_topic(mongo_collection, topic):
+    """Return list of school with specific topic."""
+    topic = mongo_collection.find({'topic': topic})
+    return list(topic)
