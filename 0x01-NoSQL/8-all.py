322c0627ee816045830bcd225e32d26001428581
root <root@DESKTOP-3SEIDIF>
2024-08-30 15:46:19 +0300

    listdocpython

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+"""Return document of collection."""
+
+
+def list_all(mongo_collection):
+    """Return the list of collection."""
+    mongo_list = []
+    for document in mongo_collection.find():
+        mongo_list.append(document)
+    return mongo_list
