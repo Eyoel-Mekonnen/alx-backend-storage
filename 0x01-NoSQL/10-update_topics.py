5cb516383c6de2c5a5b22264c174ef1341eb32e9
root <root@DESKTOP-3SEIDIF>
2024-08-30 16:52:52 +0300

    updatetopics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """Update the update topics."""
-    monog_collection.update_many(
-            {'name': name}
-            {'$set' :'topics': topics}
+    mongo_collection.update_many(
+            {'name': name},
+            {'$set': {'topics': topics}}
     )
