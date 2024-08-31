74f25dda66d5c91bb8d191dffc19c553992475e8
root <root@DESKTOP-B19RQ5P>
2024-08-31 17:29:03 +0300

    create redis object and assing value

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate random key and store the input."""
         random = str(uuid.uuid4())
         self._redis.set(random, data)
