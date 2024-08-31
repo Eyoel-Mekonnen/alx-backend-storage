75cc856c58a126d72264ae257a6b8b69b12835f6
root <root@DESKTOP-B19RQ5P>
2024-08-31 10:09:46 +0300

    lookfortopic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Return list of school with specific topic."""
-    topic = mongo_collection.find({'topic': topic})
+    topic = mongo_collection.find({'topics': topic})
     return list(topic)
